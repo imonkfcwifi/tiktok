547e95e7a6f71b4187c6772090e4cc7571436a98
imonkfcwifi <nohyungkorea@gmail.com>
2023-02-11 19:45:14 +0900

    Light Navigation

--- lib/features/main_navigation/widgets/nav_tab.dart
+++ lib/features/main_navigation/widgets/nav_tab.dart
@@ -10,37 +10,42 @@ class NavTap extends StatelessWidget {
       required this.isSelected,
       required this.icon,
       required this.onTap,
-      required this.selectedIcon});
+      required this.selectedIcon,
+      required this.selectedIndex});
 
   final String text;
   final bool isSelected;
   final IconData icon;
   final IconData selectedIcon;
   final Function onTap;
+  final int selectedIndex;
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: GestureDetector(
         onTap: () => onTap(),
-        child: AnimatedOpacity(
-          duration: const Duration(microseconds: 300),
-          opacity: isSelected ? 1 : 0.6,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              FaIcon(
-                isSelected ? selectedIcon : icon,
-                color: Colors.white,
-              ),
-              Gaps.v5,
-              Text(
-                text,
-                style: const TextStyle(
-                  color: Colors.white,
+        child: Container(
+          color: selectedIndex == 0 ? Colors.black : Colors.white,
+          child: AnimatedOpacity(
+            duration: const Duration(microseconds: 300),
+            opacity: isSelected ? 1 : 0.6,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                FaIcon(
+                  isSelected ? selectedIcon : icon,
+                  color: selectedIndex == 0 ? Colors.white : Colors.black,
                 ),
-              )
-            ],
+                Gaps.v5,
+                Text(
+                  text,
+                  style: TextStyle(
+                    color: selectedIndex == 0 ? Colors.white : Colors.black,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
