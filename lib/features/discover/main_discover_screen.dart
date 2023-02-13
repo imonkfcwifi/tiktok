df7dc4c8f3b6249ea1d4afda63bcf4b55a1d61db
imonkfcwifi <nohyungkorea@gmail.com>
2023-02-13 15:44:53 +0900

    Grid Item
    and i love totoro

--- lib/features/discover/main_discover_screen.dart
+++ lib/features/discover/main_discover_screen.dart
@@ -1,4 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+final tabs = [
+  "Top",
+  "Users",
+  "Videos",
+  "Sounds",
+  "LIVE",
+  "Shopping",
+  "Brands",
+];
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
@@ -6,32 +19,105 @@ class DiscoverScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 4,
+      length: tabs.length,
       child: Scaffold(
         appBar: AppBar(
+          elevation: 1,
           title: const Text('discover'),
           bottom: TabBar(
+            splashFactory: NoSplash.splashFactory,
+            padding: const EdgeInsets.symmetric(horizontal: Sizes.size16),
+            isScrollable: true,
             labelStyle: const TextStyle(
+              fontSize: Sizes.size16,
               fontWeight: FontWeight.w400,
             ),
             indicatorColor: Colors.black,
             unselectedLabelColor: Colors.grey.shade500,
             labelColor: Colors.black,
-            tabs: const [
-              Tab(
-                text: "Top",
-              ),
-              Tab(
-                text: "Top",
-              ),
-              Tab(
-                text: "Top",
-              ),
-              Tab(
-                text: "Top",
-              )
+            tabs: [
+              for (var tab in tabs) Tab(text: tab),
             ],
           ),
+        ),
+        body: TabBarView(
+          children: [
+            GridView.builder(
+              itemCount: 20,
+              padding: const EdgeInsets.all(Sizes.size10),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: Sizes.size10,
+                mainAxisSpacing: Sizes.size10,
+                childAspectRatio: 9 / 20,
+              ),
+              itemBuilder: (context, index) => Column(
+                children: [
+                  AspectRatio(
+                    aspectRatio: 9 / 16,
+                    child: FadeInImage.assetNetwork(
+                      fit: BoxFit.cover,
+                      placeholder: "assets.images/placeholder.jpg",
+                      image:
+                          "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.leagueoflegends.com%2Fko-kr%2Fevent%2Fk-sante-ability-rundown%2F&psig=AOvVaw2NHQ1NuzAlPwJc7PXfG7Xz&ust=1676356112482000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCKjVvqbvkf0CFQAAAAAdAAAAABAE",
+                    ),
+                  ),
+                  Gaps.v10,
+                  const Text(
+                    "This is a very long caption for my that im upload just now currently",
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                    style: TextStyle(
+                      fontSize: Sizes.size16 + Sizes.size2,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Gaps.v5,
+                  DefaultTextStyle(
+                    style: TextStyle(
+                      color: Colors.grey.shade600,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    // DefaultTextStyle을 지정함으로서 이 안에있는 모든 자식 Text가 같은 style을 갖게 됨
+                    child: Row(
+                      children: [
+                        const CircleAvatar(
+                          radius: 15,
+                          backgroundImage: NetworkImage(
+                              "https://i.ytimg.com/vi/Z5ajy2H1vw8/maxresdefault.jpg"),
+                        ),
+                        Gaps.h8,
+                        const Expanded(
+                          child: Text(
+                            "lalalalakakakaka",
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        Gaps.h8,
+                        FaIcon(
+                          FontAwesomeIcons.heart,
+                          size: Sizes.size16,
+                          color: Colors.grey.shade600,
+                        ),
+                        Gaps.h2,
+                        const Text(
+                          "2.5M",
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            for (var tab in tabs.skip(1))
+              Center(
+                child: Text(
+                  tab,
+                  style: const TextStyle(fontSize: 28),
+                ),
+              ),
+          ],
         ),
       ),
     );
