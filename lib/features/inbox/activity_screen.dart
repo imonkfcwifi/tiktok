cf74a6d78d7f979ecd4ba17a7d14413151edb718
imonkfcwifi <nohyungkorea@gmail.com>
2023-02-14 16:25:42 +0900

    Animation<Offset>
    SlideTransition

--- lib/features/inbox/activity_screen.dart
+++ lib/features/inbox/activity_screen.dart
@@ -18,13 +18,45 @@ class _ActivityScreenState extends State<ActivityScreen>
     _notification.remove(notification);
   }
 
+  final List<Map<String, dynamic>> _tabs = [
+    {
+      "title": "All activity",
+      "icon": FontAwesomeIcons.solidMessage,
+    },
+    {
+      "title": "Likes",
+      "icon": FontAwesomeIcons.solidHeart,
+    },
+    {
+      "title": "Comments",
+      "icon": FontAwesomeIcons.solidComments,
+    },
+    {
+      "title": "Mentions",
+      "icon": FontAwesomeIcons.at,
+    },
+    {
+      "title": "Followers",
+      "icon": FontAwesomeIcons.solidUser,
+    },
+    {
+      "title": "From TikTok",
+      "icon": FontAwesomeIcons.tiktok,
+    }
+  ];
+
   late final AnimationController _animationController = AnimationController(
       vsync: this, duration: const Duration(milliseconds: 300));
-
   // init 안해도 됨
 
-  late final Animation<double> _animation =
+  late final Animation<double> _arrowAnimation =
       Tween(begin: 0.0, end: 0.5).animate(_animationController);
+
+  late final Animation<Offset> _pannelAnimation = Tween(
+    begin: const Offset(0, -1),
+    end: Offset.zero,
+    // 비율로 생각해야함 (not a pixels)
+  ).animate(_animationController);
 
   void _onTitleTap() {
     if (_animationController.isCompleted) {
@@ -52,7 +84,7 @@ class _ActivityScreenState extends State<ActivityScreen>
               const Text("All activity"),
               Gaps.h2,
               RotationTransition(
-                turns: _animation,
+                turns: _arrowAnimation,
                 child: const FaIcon(
                   FontAwesomeIcons.chevronDown,
                   size: Sizes.size14,
@@ -62,86 +94,127 @@ class _ActivityScreenState extends State<ActivityScreen>
           ),
         ),
       ),
-      body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: Sizes.size20),
+      body: Stack(
         children: [
-          Gaps.v12,
-          const Text(
-            "New",
-            style: TextStyle(fontSize: Sizes.size14, color: Colors.grey),
-          ),
-          Gaps.v14,
-          for (var notification in _notification)
-            Dismissible(
-              onDismissed: (notification) => _onDismissed,
-              key: Key(notification),
-              // item들이 각각 다른 notification 을 갖게됨 (unique key)
-              background: Container(
-                alignment: Alignment.centerLeft,
-                // container 안에 있는 아이템 정렬
-                color: Colors.green,
-                child: const Padding(
-                  padding: EdgeInsets.only(left: Sizes.size10),
-                  child: FaIcon(FontAwesomeIcons.airbnb),
-                ),
+          ListView(
+            padding: const EdgeInsets.symmetric(horizontal: Sizes.size20),
+            children: [
+              Gaps.v12,
+              const Text(
+                "New",
+                style: TextStyle(fontSize: Sizes.size14, color: Colors.grey),
               ),
-              secondaryBackground: Container(
-                alignment: Alignment.centerRight,
-                // container 안에 있는 아이템 정렬
-                color: Colors.red,
-                child: const Padding(
-                  padding: EdgeInsets.only(right: Sizes.size10),
-                  child: FaIcon(FontAwesomeIcons.airbnb),
-                ),
-              ),
-              child: ListTile(
-                minVerticalPadding: Sizes.size16,
-                trailing: const FaIcon(
-                  FontAwesomeIcons.chevronRight,
-                  size: Sizes.size32,
-                  color: Colors.white,
-                ),
-                contentPadding: EdgeInsets.zero,
-                leading: Container(
-                  width: Sizes.size52,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.white,
-                    border: Border.all(
-                        color: Colors.grey.shade400, width: Sizes.size1),
-                  ),
-                  child: const Center(
-                    child: FaIcon(
-                      FontAwesomeIcons.bell,
-                      color: Colors.black,
+              Gaps.v14,
+              for (var notification in _notification)
+                Dismissible(
+                  onDismissed: (notification) => _onDismissed,
+                  key: Key(notification),
+                  // item들이 각각 다른 notification 을 갖게됨 (unique key)
+                  background: Container(
+                    alignment: Alignment.centerLeft,
+                    // container 안에 있는 아이템 정렬
+                    color: Colors.green,
+                    child: const Padding(
+                      padding: EdgeInsets.only(left: Sizes.size10),
+                      child: FaIcon(FontAwesomeIcons.airbnb),
                     ),
                   ),
-                ),
-                title: RichText(
-                  text: TextSpan(
-                    text: "Account Updates",
-                    style: const TextStyle(
-                      fontWeight: FontWeight.w600,
-                      color: Colors.black,
+                  secondaryBackground: Container(
+                    alignment: Alignment.centerRight,
+                    // container 안에 있는 아이템 정렬
+                    color: Colors.red,
+                    child: const Padding(
+                      padding: EdgeInsets.only(right: Sizes.size10),
+                      child: FaIcon(FontAwesomeIcons.airbnb),
                     ),
-                    children: [
-                      const TextSpan(
-                        text: " Upload longer video",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w100,
+                  ),
+                  child: ListTile(
+                    minVerticalPadding: Sizes.size16,
+                    trailing: const FaIcon(
+                      FontAwesomeIcons.chevronRight,
+                      size: Sizes.size32,
+                      color: Colors.white,
+                    ),
+                    contentPadding: EdgeInsets.zero,
+                    leading: Container(
+                      width: Sizes.size52,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
+                        border: Border.all(
+                            color: Colors.grey.shade400, width: Sizes.size1),
+                      ),
+                      child: const Center(
+                        child: FaIcon(
+                          FontAwesomeIcons.bell,
+                          color: Colors.black,
                         ),
                       ),
-                      TextSpan(
-                        text: " $notification",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w100,
-                            color: Colors.grey.shade500),
+                    ),
+                    title: RichText(
+                      text: TextSpan(
+                        text: "Account Updates",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black,
+                        ),
+                        children: [
+                          const TextSpan(
+                            text: " Upload longer video",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w100,
+                            ),
+                          ),
+                          TextSpan(
+                            text: " $notification",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w100,
+                                color: Colors.grey.shade500),
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
                 ),
+            ],
+          ),
+          SlideTransition(
+            position: _pannelAnimation,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(Sizes.size4),
+                  bottomRight: Radius.circular(Sizes.size4),
+                ),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                // row가 가로를 전부 차지하려는 거 만큼 column도 똑같기때문에 mainaxisSize를 minimum
+                children: [
+                  for (var tab in _tabs)
+                    ListTile(
+                      title: Row(
+                        children: [
+                          FaIcon(
+                            tab["icon"],
+                            color: Colors.black,
+                            size: Sizes.size16,
+                          ),
+                          Gaps.h20,
+                          Text(
+                            tab["title"],
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                ],
               ),
             ),
+          )
         ],
       ),
     );
