5c593f1f945fa7dc83552db8c797c40ac26c19f4
imonkfcwifi <nohyungkorea@gmail.com>
2023-02-20 21:32:27 +0900

    showDateRangePicker

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -12,13 +12,34 @@ class SettingScreen extends StatelessWidget {
         body: ListView(
           children: [
             ListTile(
-              onTap: () => showAboutDialog(
+              onTap: () async {
+                final date = await showDatePicker(
                   context: context,
-                  applicationVersion: "1.0",
-                  applicationLegalese:
-                      "All rightts reserved. Please dont copy me."),
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(1980),
+                  lastDate: DateTime(2030),
+                );
+                print(date);
+                final time = await showTimePicker(
+                    context: context, initialTime: TimeOfDay.now());
+                print(time);
+                final booking = await showDateRangePicker(
+                  context: context,
+                  firstDate: DateTime(1980),
+                  lastDate: DateTime(2030),
+                  builder: (context, child) {
+                    return Theme(
+                      data: ThemeData(
+                          appBarTheme:
+                              const AppBarTheme(foregroundColor: Colors.white),
+                          backgroundColor: Colors.black),
+                      child: child!,
+                    );
+                  },
+                );
+              },
               title: const Text(
-                'data',
+                'what is your birthday',
                 style: TextStyle(fontWeight: FontWeight.w600),
               ),
               subtitle: const Text('About this ap..'),
