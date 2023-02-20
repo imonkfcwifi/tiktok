4505426e63cfb68304189de10f0917813a5a31fe
imonkfcwifi <nohyungkorea@gmail.com>
2023-02-20 22:01:38 +0900

    AlertDialog

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
@@ -87,6 +88,55 @@ class _SettingScreenState extends State<SettingScreen> {
               ),
               subtitle: const Text('About this ap..'),
             ),
+            ListTile(
+                title: const Text(
+                  "data (iOS) ",
+                ),
+                textColor: Colors.red,
+                onTap: () {
+                  showCupertinoDialog(
+                    context: context,
+                    builder: (context) => CupertinoAlertDialog(
+                      title: const Text("data"),
+                      content: const Text("data2"),
+                      actions: [
+                        CupertinoDialogAction(
+                          onPressed: () => Navigator.of(context).pop(),
+                          child: const Text("No"),
+                        ),
+                        CupertinoDialogAction(
+                          onPressed: () => Navigator.of(context).pop(),
+                          isDestructiveAction: true,
+                          child: const Text("Yes"),
+                        ),
+                      ],
+                    ),
+                  );
+                }),
+            ListTile(
+                title: const Text(
+                  "data (android) ",
+                ),
+                textColor: Colors.red,
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: const Text("data"),
+                      content: const Text("data2"),
+                      actions: [
+                        IconButton(
+                          onPressed: () => Navigator.of(context).pop(),
+                          icon: const FaIcon(FontAwesomeIcons.car),
+                        ),
+                        TextButton(
+                          onPressed: () => Navigator.of(context).pop(),
+                          child: const Text("Yes"),
+                        ),
+                      ],
+                    ),
+                  );
+                }),
             const AboutListTile()
           ],
         ));
