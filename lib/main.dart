66ec86812e8a7b21be60f1ee23b3d2cc496b4659
imonkfcwifi <nohyungkorea@gmail.com>
2023-02-22 21:32:41 +0900

    Google Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -30,37 +30,7 @@ class TikTokApp extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: ThemeData(
         brightness: Brightness.light,
-        textTheme: TextTheme(
-          displayLarge: GoogleFonts.raleway(
-              fontSize: 99, fontWeight: FontWeight.w300, letterSpacing: -1.5),
-          displayMedium: GoogleFonts.raleway(
-              fontSize: 62, fontWeight: FontWeight.w300, letterSpacing: -0.5),
-          displaySmall:
-              GoogleFonts.raleway(fontSize: 49, fontWeight: FontWeight.w400),
-          headlineMedium: GoogleFonts.raleway(
-              fontSize: 35, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-          headlineSmall:
-              GoogleFonts.raleway(fontSize: 25, fontWeight: FontWeight.w400),
-          titleLarge: GoogleFonts.raleway(
-              fontSize: 21, fontWeight: FontWeight.w500, letterSpacing: 0.15),
-          titleMedium: GoogleFonts.raleway(
-              fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.15),
-          titleSmall: GoogleFonts.raleway(
-              fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
-          bodyLarge: GoogleFonts.roboto(
-              fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
-          bodyMedium: GoogleFonts.roboto(
-              fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-          labelLarge: GoogleFonts.roboto(
-              fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
-          bodySmall: GoogleFonts.roboto(
-              fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
-          labelSmall: GoogleFonts.roboto(
-              fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
-        ),
-        // 이제 textstyle을 줄때 (app 첫 구상시) 위와 같은 구성으로 맞추는게 편할듯
-        // we dont need to think about sizes
-        // property 넣고싶을때 ex ) !.copyWith(color:Colors.red)
+        textTheme: GoogleFonts.itimTextTheme(),
         splashColor: Colors.transparent,
         primaryColor: const Color(0xFFE9435A),
         textSelectionTheme:
@@ -79,6 +49,9 @@ class TikTokApp extends StatelessWidget {
         ),
       ),
       darkTheme: ThemeData(
+        textTheme: GoogleFonts.itimTextTheme(
+          ThemeData(brightness: Brightness.dark).textTheme,
+        ),
         brightness: Brightness.dark,
         scaffoldBackgroundColor: Colors.black,
         primaryColor: const Color(0xFFE9435A),
