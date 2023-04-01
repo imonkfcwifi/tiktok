bbf0fab74c65c40836b2df97ee7db0e569a6e9bd
imonkfcwifi <nohyungkorea@gmail.com>
2023-04-01 15:11:33 +0900

    Update avatar.dart

--- lib/features/users/widgets/avatar.dart
+++ lib/features/users/widgets/avatar.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:tiktok_clone/features/users/view_models/avatar_view_model.dart';
 
 class Avatar extends ConsumerWidget {
   final String name;
@@ -12,7 +13,7 @@ class Avatar extends ConsumerWidget {
     required this.name,
   });
 
-  Future<void> _onAvatarTap() async {
+  Future<void> _onAvatarTap(WidgetRef ref) async {
     final xfile = await ImagePicker().pickImage(
       source: ImageSource.gallery,
       imageQuality: 40,
@@ -21,19 +22,31 @@ class Avatar extends ConsumerWidget {
     );
     if (xfile != null) {
       final file = File(xfile.path);
+      ref.read(avatarProvider.notifier).uploadVatar(file);
     }
   }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final isLoading = ref.watch(avatarProvider).isLoading;
     return GestureDetector(
-      onTap: _onAvatarTap,
-      child: CircleAvatar(
-        radius: 50,
-        foregroundImage: const NetworkImage(
-            "https://avatars.githubusercontent.com/u/3612017"),
-        child: Text(name),
-      ),
+      onTap: isLoading ? null : () => _onAvatarTap,
+      child: isLoading
+          ? Container(
+              width: 50,
+              height: 50,
+              alignment: Alignment.center,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+              ),
+              child: const CircularProgressIndicator(),
+            )
+          : CircleAvatar(
+              radius: 50,
+              foregroundImage: const NetworkImage(
+                  "https://avatars.githubusercontent.com/u/3612017"),
+              child: Text(name),
+            ),
     );
   }
 }
